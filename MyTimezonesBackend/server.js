18430bade65d8aaf6cf8299630dbab79b027e7c7
Carlos Ginés <efrel@MacBook-Pro-de-Efrel.local>
2016-04-04 21:45:34 +0200

    Backend: Added .gitignore Added package.son Added express module

--- MyTimezonesBackend/server.js
+++ MyTimezonesBackend/server.js
@@ -1,9 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response) {
-	response.writeHead(200);
-	response.write("Hello Toptaler!");
-	response.end();
-}).listen(8080, '0.0.0.0', function(){
+app.get('/', function(req, res) {
+	res.send('Hello Toptaler');
+});
+
+app.listen(8080, '0.0.0.0', function(){
 	console.log('Listening on port 8080...');
 });
