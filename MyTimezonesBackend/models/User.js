f7d8944edd7c4c70bfd75e3c008c9367ee39fe75
Carlos Ginés <efrel@MacBook-Pro-de-Efrel.local>
2016-04-07 19:16:58 +0200

    Backend: Removed Mongoose version keys from collections.

--- MyTimezonesBackend/models/User.js
+++ MyTimezonesBackend/models/User.js
@@ -1,29 +1,34 @@
 var mongoose = require('mongoose');
 
-module.exports = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true
+module.exports = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    },
+    created: {
+      type: Date,
+      default: Date.now
+    },
+    updated:  {
+      type: Date,
+      default: Date.now
+    }
   },
-  admin: {
-    type: Boolean,
-    default: false,
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  token: {
-    type: String,
-    required: true
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  updated:  {
-    type: Date,
-    default: Date.now
-  },
-});
+  {
+    versionKey: false
+  }
+);
